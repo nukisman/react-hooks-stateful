1e64622f8e318f8e0328d479e3c1e32479eef307
nukisman <nukisman@gmail.com>
2019-09-26 15:41:24 +0300

    Module typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,16 +2,24 @@
  * Default CSS definition for typescript,
  * will be overridden with file-specific definitions by rollup
  */
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-interface ISvgrComponent extends React.FC<React.SVGAttributes<SVGElement>> {}
-
-declare module '*.svg' {
-  const svgUrl: string;
-  const svgComponent: ISvgrComponent;
-  export default svgUrl;
-  export { svgComponent as ReactComponent };
-}
+// declare module '*.css' {
+//   const content: { [className: string]: string };
+//   export default content;
+// }
+//
+// interface ISvgrComponent extends React.FC<React.SVGAttributes<SVGElement>> {}
+//
+// declare module '*.svg' {
+//   const svgUrl: string;
+//   const svgComponent: ISvgrComponent;
+//   export default svgUrl;
+//   export { svgComponent as ReactComponent };
+// }
+//
+// declare module '*.svg';
+// declare module '*.png';
+// declare module '*.jpg';
+// declare module '*.jpeg';
+// declare module '*.gif';
+// declare module '*.bmp';
+// declare module '*.tiff';
